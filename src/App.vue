914b37fe9deb947646eedd16d463ec208e45dafb
Lisandro <lucattilisandro@gmail.com>
2022-06-17 21:01:15 -0300

    [ADD] project structure

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink style="margin-left: 20px" to="/show/1">Show details</RouterLink>
+  <RouterView /> 
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>This is the home Page</h1>
+</template>
