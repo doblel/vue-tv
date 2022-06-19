bc3d145d13b29169622131755cfb1d280225b59e
Lisandro <lucattilisandro@gmail.com>
2022-06-19 03:32:51 -0300

    [ADD] UI Components

---
+++ src/components/ShowCard.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <RouterLink
+    :to="`/show/${id}`"
+  >
+    <img :src="image" class="show-card" loading="lazy" :alt="`${name} image`">
+    <span class="sr-only">{{ name }}</span>
+  </RouterLink>
+</template>
+
+<style scoped>
+@import "@/assets/base.css";
+ 
+.show-card {
+  display: inline-block;
+  flex-shrink: 0;
+  margin-right: 10px;
+  border-radius: var(--border-radius);
+  transition: all 0.2s;
+  width: 120px;
+  height: 160px;
+}
+
+.show-card:hover {
+  transform: scale(1.03);
+}
+
+@media (min-width: 860px) {
+  .show-card {
+    margin-right: 20px;
+    height: 300px;
+    width: 200px;
+  }
+}
+</style>
